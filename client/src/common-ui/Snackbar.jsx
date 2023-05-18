b3f97074c9198af993ea8ec76660010a2cba3e82
thedeepanshu <deepanshusaini971@gmail.com>
2023-05-18 22:48:29 +0530

    ✨Added snackbar for future use

---
+++ client/src/common-ui/Snackbar.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+const Snackbar = () => {
+  return (
+    <div className="mt-10 fixed z-10 bottom-0 left-0 right-0">
+      {/* Error */}
+      <div
+        id="alert-border-2"
+        className="flex p-4 text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:border-red-800"
+        role="alert"
+      >
+        <svg
+          className="flex-shrink-0 w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+        <div className="ml-3 text-sm font-medium">Error Message</div>
+        <button
+          type="button"
+          className="ml-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex h-8 w-8 dark:text-red-400 dark:hover:bg-red-150"
+          data-dismiss-target="#alert-border-2"
+          aria-label="Close"
+        >
+          <span className="sr-only">Dismiss</span>
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </div>
+
+      {/* Success */}
+      <div
+        id="alert-border-3"
+        className="flex p-4 text-green-800 border-t-4 border-green-300 bg-green-50 dark:text-green-400 dark:border-green-800"
+        role="alert"
+      >
+        <svg
+          className="flex-shrink-0 w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+        <div className="ml-3 text-sm font-medium">Success Message</div>
+        <button
+          type="button"
+          className="ml-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex h-8 w-8 dark:text-green-400 dark:hover:bg-green-150"
+          data-dismiss-target="#alert-border-3"
+          aria-label="Close"
+        >
+          <span className="sr-only">Dismiss</span>
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Snackbar;
